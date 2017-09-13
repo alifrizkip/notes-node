d01c5dc712a30886a681e8fb38c9ed959e30ff51
alip <alifrizkipambudi@gmail.com>
2017-09-13 15:43:41 +0700

    After nodemon

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes');
 const _ = require('lodash');
 
-console.log(_.isString(true));
-console.log(_.isString('Alif'));
-
-//console.log('Result: ', notes.add(10, -2));
-
-// fs.appendFile('greetings.txt', `Hello ${user.username}.You are ${age} years old!`, function (err) {
-//     if (err) {
-//         console.log('Unable to write file');
-//     }
-// });
+const notes = require('./notes');
--- notes.js
+++ notes.js
@@ -1,10 +1,1 @@
 console.log('Starting notes.js');
-
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New Note';
-}
-
-module.exports.add = (a, b) => {
-    return a + b;
-}
