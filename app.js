f2e7d6a9dcc04f43126b9a29dad2d578de7a3b1b
alip <alifrizkipambudi@gmail.com>
2017-09-13 21:05:12 +0700

    Install yargs module

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes');
 
+const argv = yargs.argv;
 var command = process.argv[2];
 console.log('Command: ', command);
+console.log('Process :', process.argv);
+console.log('Yargs :', argv);
 
 if (command === 'add') {
-    console.log('Adding new note');
+    notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
     console.log('Listing all notes');
 } else if (command === 'read') {
--- notes.js
+++ notes.js
@@ -1,6 +1,9 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New Note';
+var addNote = (title, body) => {
+    console.log('Adding note :', title, body);
+}
+
+module.exports = {
+    addNote
 }
