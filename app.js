d5969e0b3b144503dd03768cf8bdbca3f04c5ea4
alip <alifrizkipambudi@gmail.com>
2017-09-13 20:44:05 +0700

    Learn input arguments

--- app.js
+++ app.js
@@ -4,3 +4,18 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes');
+
+var command = process.argv[2];
+console.log('Command: ', command);
+
+if (command === 'add') {
+    console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} else if (command === 'read') {
+    console.log('Reading a note');
+} else if (command === 'remove') {
+    console.log('Removing a note');
+} else {
+    console.log('Command not recognize');
+}
